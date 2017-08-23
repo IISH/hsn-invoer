7a11afe193ee3d2cbb15feef4ebfb0d0afdc7714
kerim1 <kerim.meijer@gmail.com>
2017-08-23 10:28:14 +0200

    Disable the submit buttons after a submit

--- src/main/resources/static/js/navigation.js
+++ src/main/resources/static/js/navigation.js
@@ -371,8 +371,15 @@
         initTabIndexes();
     });
 
-    $('form:not(.noResetOnSubmit)').submit(function () {
-        $(document).resetInvisibleFormElements();
+    $('form').submit(function () {
+        var form = $(this);
+        var activeElement = document.activeElement;
+        form.find('button[type="submit"]').addClass('submit-disable').prop('disabled', true);
+        $(activeElement).focus();
+
+        if (form.is(':not(.noResetOnSubmit)')) {
+            $(document).resetInvisibleFormElements();
+        }
     });
 
     $(document).ready(function () {
